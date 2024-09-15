a831c8c6022a8189320067489fd63023818e45af
Daniel Babalola <79989918+Dannny-Babs@users.noreply.github.com>
2024-09-15 00:46:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react(),
-                ],
-   rollupOptions: {
-      external: ["@/assets/talo-link-image.svg"],
-    },
+  integrations: [tailwind(), icon(), react()],
+  rollupOptions: {
+    external: ['@/assets/talo-link-image.svg'],
+  },
 });
