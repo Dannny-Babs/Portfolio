6694807ff90a4ee3469cad8575f34f059d60c106
Daniel <dannybabs123456@gmail.com>
2024-09-15 01:33:21 -0400

    Update astro.config.mjs and ContactMe.astro
    - Update astro.config.mjs to add the build.rollupOptions configuration for Vite.
    - Update ContactMe.astro and ContactSesction.astro to change the background color of input fields.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,25 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
-  build: {
-    rollupOptions: {
-      external: ['@/assets/talo-link-image.svg']
-    }
-  }
+
+  // Add the build.rollupOptions configuration
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    build: {
+      rollupOptions: {
+        external: ["/@assets/talo-link-image.svg"],
+      },
+    },
+  },
 });
